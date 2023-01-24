ef16b1c1166710620a2e76f46c6bddc50653b0b6
MubashirJ <mubashirmjjawed@gmail.com>
2023-01-24 21:59:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const acc = document.querySelectorAll(".accordion-title");
+
+acc.forEach((elem) => {
+  elem.addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+});
